cd8953f588b8719e407d5bbaa8208631ec431ce0
Michael Herrmann <michael@herrmann.io>
2019-04-25 09:14:29 +0200

    Clean up code

--- 06 Text Editor/main.py
+++ 06 Text Editor/main.py
@@ -1,82 +1,81 @@
-from os.path import basename
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+from PyQt5.QtGui import QKeySequence
 
 class MainWindow(QMainWindow):
-    def closeEvent(self, event):
-        if text.document().isModified():
-            answer = QMessageBox.question(
-                window, 'Text Editor',
-                'You have unsaved changes. Save before closing?',
-                QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel
-            )
-            if answer & QMessageBox.Save:
-                save()
-            elif answer & QMessageBox.Cancel:
-                event.ignore()
+    def closeEvent(self, e):
+        if not text.document().isModified():
+            return
+        answer = QMessageBox.question(
+            window, None,
+            "You have unsaved changes. Save before closing?",
+            QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel
+        )
+        if answer & QMessageBox.Save:
+            save()
+        elif answer & QMessageBox.Cancel:
+            e.ignore()
 
 app = QApplication([])
+app.setApplicationName("Text Editor")
+text = QPlainTextEdit()
 window = MainWindow()
-text = QTextEdit()
 window.setCentralWidget(text)
-window.setWindowTitle('Text Editor')
 
 file_path = None
 
-def save_as():
-    global file_path
-    new_path = QFileDialog.getSaveFileName(window, 'Save As', file_path)[0]
-    if new_path:
-        file_path = new_path
-        save()
-
-def save():
-    if not file_path:
-        save_as()
-    else:
-        with open(file_path, 'w') as f:
-            f.write(text.toPlainText())
-        text.document().setModified(False)
-
+menu = window.menuBar().addMenu("&File")
+open_action = QAction("&Open")
 def open_file():
     global file_path
-    path = QFileDialog.getOpenFileName(window, 'Open', file_path)[0]
+    path = QFileDialog.getOpenFileName(window, "Open")[0]
     if path:
         text.setPlainText(open(path).read())
         file_path = path
-
-file_menu = window.menuBar().addMenu("&File")
-
-open_action = QAction("&Open")
-open_action.setShortcut(QKeySequence.Open)
 open_action.triggered.connect(open_file)
-file_menu.addAction(open_action)
+open_action.setShortcut(QKeySequence.Open)
+menu.addAction(open_action)
 
 save_action = QAction("&Save")
-save_action.setShortcut(QKeySequence.Save)
+def save():
+    if file_path is None:
+        save_as()
+    else:
+        with open(file_path, "w") as f:
+            f.write(text.toPlainText())
+        text.document().setModified(False)
 save_action.triggered.connect(save)
-file_menu.addAction(save_action)
+save_action.setShortcut(QKeySequence.Save)
+menu.addAction(save_action)
 
 save_as_action = QAction("Save &As...")
+def save_as():
+    global file_path
+    path = QFileDialog.getSaveFileName(window, "Save As")[0]
+    if path:
+        file_path = path
+        save()
 save_as_action.triggered.connect(save_as)
-file_menu.addAction(save_as_action)
+menu.addAction(save_as_action)
 
-exit_action = QAction("&Close")
-exit_action.triggered.connect(window.close)
-file_menu.addAction(exit_action)
+close = QAction("&Close")
+close.triggered.connect(window.close)
+menu.addAction(close)
 
 help_menu = window.menuBar().addMenu("&Help")
-
 about_action = QAction("&About")
 help_menu.addAction(about_action)
-help_menu.triggered.connect(lambda: QMessageBox.about(window, 'About Text Editor', '<center>'
-            '<h1>Text Editor</h1>&zwnj;'
-            '<img src="icon.png">'
-            '</center>'
-            '<p>Version 31.4.159.265358<br/>'
-            'Copyright &copy; Company Inc.</p>'))
+def show_about_dialog():
+    QMessageBox.about(
+        window, "About Text Editor",
+        "<center>"
+        "<h1>Text Editor</h1>"
+        "&#8291;" # Force a line break
+        "<img src=icon.png>"
+        "</center>"
+        "<p>Version 31.4.159.265358<br/>"
+        "Copyright &copy; Company Inc.</p>"
+    )
+about_action.triggered.connect(show_about_dialog)
 
 window.show()
-
 app.exec_()
