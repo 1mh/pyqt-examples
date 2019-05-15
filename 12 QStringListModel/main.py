2d2b263c5f82c4839cace0a4d2a964571b7448ec
Michael Herrmann <michael@herrmann.io>
2019-05-15 10:24:08 +0200

    Improve example

--- 12 QStringListModel/main.py
+++ 12 QStringListModel/main.py
@@ -2,7 +2,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QStringListModel
 
 app = QApplication([])
-model = QStringListModel(["H", "e", "l", "l", "o", "!"])
+model = QStringListModel([
+    "An element", "Another element", "Yay! Another one."
+])
 view = QListView()
 view.setModel(model)
 view.show()
