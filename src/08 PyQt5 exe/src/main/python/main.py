640adc0c50f266a2ed7085323b563b35ff3dbd32
Michael Herrmann <michael@herrmann.io>
2019-06-10 13:37:19 +0200

    Update to fbs 0.8.3

--- src/08 PyQt5 exe/src/main/python/main.py
+++ src/08 PyQt5 exe/src/main/python/main.py
@@ -1,4 +1,4 @@
-from fbs_runtime.application_context import ApplicationContext
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow
 
 import sys
